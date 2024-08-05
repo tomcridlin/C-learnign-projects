9bfc407d2b1a31573c25e4b7b6e2b05f24235e03
Tom <tomcridlin@gmail.com>
2024-08-05 02:00:16 +0200

    Aritmatic added

---
+++ Arithmatic operators/basicAritmatic.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main(){
+    int students = 20;
+
+    //students = students + 1;
+    //students+=1;
+    //students++;
+
+    //students = students -2;
+    //students-=2;
+    //students--; Only Subtracts 1
+
+    //students = students * 2;
+    //students*=2;
+
+    //students = students / 2;
+    //students/=2;
+
+    //int remainder = students % 3;
+    
+    std::cout << students;
+
+    return 0;
+
+}
