22b6926259bd8e73344bbdccb5f587717c68bb3e
Tom <tomcridlin@gmail.com>
2024-08-05 03:03:45 +0200

    Age tester added

---
+++ If Statements/IFStatements.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main(){
+
+
+    int age;
+
+    std::cout << "Enter your age: ";
+    std::cin >> age;
+
+    if(age > 60){
+        std::cout << "I dont want to break your hip!";
+    }
+    else if(age >= 18 ){
+        std::cout << "Smash";
+    }
+    else if(age <=  0){
+        std::cout << "You havent even been born yet!";
+    }
+    else{
+        std::cout << "Please go away child";
+    }
+
+
+
+    return 0;
+}
